19e5648f8d37da4c35d857c6ed0004e8b10604c4
zLeggi <leandrosalmeida200021@gmail.com>
2022-08-18 14:56:13 -0300

    Adicionado novo mini projeto - Contador

---
+++ Contador/assets/js/script.js
@@ -0,0 +1,14 @@
+var currentNumberWapper = document.getElementById('currentNumber')
+var currentNumber = 0;
+
+function increment() {
+
+  currentNumber = currentNumber + 1;
+  currentNumberWapper.innerHTML = currentNumber;
+
+}
+
+function decrement() {
+  currentNumber = currentNumber - 1;
+  currentNumberWapper.innerHTML = currentNumber;
+}
